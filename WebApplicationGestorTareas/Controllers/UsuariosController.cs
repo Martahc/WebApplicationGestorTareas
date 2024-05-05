b59147260719e3fe899e7ab2457cba9a20edb38a
Martahc <martahica@gmail.com>
2024-05-05 09:14:37 +0200

    Modificada la logica de los procesos de Crear, Asignar, Completar y Eliminar una tarea #6

--- WebApplicationGestorTareas/Controllers/UsuariosController.cs
+++ WebApplicationGestorTareas/Controllers/UsuariosController.cs
@@ -309,7 +309,6 @@ namespace WebApplicationGestorTareas.Controllers
         }
 
 
-
         [HttpPost]
         [ValidateAntiForgeryToken]
         public ActionResult MarcarTareaCompletada(int idUsuario, int idTarea, string estadoTarea)
@@ -326,20 +325,9 @@ namespace WebApplicationGestorTareas.Controllers
                 return HttpNotFound();
             }
 
-            if (usuario.Puntos.HasValue && tarea.Puntos.HasValue)
-            {
-                usuario.Puntos += tarea.Puntos;
-            }
-            else if (!usuario.Puntos.HasValue && tarea.Puntos.HasValue)
-            {
-                usuario.Puntos = tarea.Puntos;
-            }
-
             tarea.Estado = estadoTarea;
             tarea.FechaFin = DateTime.Now;
-
-            int plazo = tarea.Plazo.Value;
-
+           
             DateTime fechaLimite = tarea.FechaInicio.Value.AddDays(tarea.Plazo.Value);
 
             if (tarea.FechaFin > fechaLimite)
@@ -351,6 +339,15 @@ namespace WebApplicationGestorTareas.Controllers
             }
             else
             {
+                if (usuario.Puntos.HasValue && tarea.Puntos.HasValue)
+                {
+                    usuario.Puntos += tarea.Puntos;
+                }
+                else if (!usuario.Puntos.HasValue && tarea.Puntos.HasValue)
+                {
+                    usuario.Puntos = tarea.Puntos;
+                }
+
                 var premio = db.Premio
                 .Where(p => p.Puntos <= usuario.Puntos)
                 .OrderByDescending(p => p.Puntos)
@@ -359,8 +356,10 @@ namespace WebApplicationGestorTareas.Controllers
                 if (premio != null)
                 {
                     usuario.Premio.Add(premio);
-                    premio.Usuario.Add(usuario);
+                    premio.Usuario.Add(usuario);                   
                     db.Entry(premio).State = EntityState.Modified;
+
+                    usuario.Puntos -= premio.Puntos;
                 }
             }
 
